1e8e97b895fb299043cebddc3c8e4ca7f205175e
shar-c0des <ngomakablesharleen703@gmail.com>
2025-10-14 13:24:50 +0200

    fixed Pubspec file to use 'Roomantics' not 'roomanties'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:roomanties/main.dart';
-import 'package:roomanties/views/pages/splash_screen_page.dart';
+import 'package:Roomantics/main.dart';
+import 'package:Roomantics/views/pages/splash_screen_page.dart';
 
 void main() {
   testWidgets('RoomanticApp loads initial SplashScreenPage', (WidgetTester tester) async {
